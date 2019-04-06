1b18a6749daa540f06a1ec39260a68032d5e4858
ymj <1297905249@qq.com>
2019-04-06 10:29:19 +0800

    pull request好像

--- src/ScondeTest.java
+++ src/ScondeTest.java
@@ -1,5 +1,23 @@
 public class ScondeTest {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        /*
+        * 问题1：pull request的作用
+
+比如在仓库的主人（A）没有把我们添加为项目合作者的前提下，我们将A的某个仓库名为“B”的仓库clone到自己的电脑中，在自己的电脑进行修改，但是我们会发现我们没办法通过push将代码贡献到B中。
+
+所以要想将你的代码贡献到B中，我们应该：
+
+在A的仓库中fork项目B （此时我们自己的github就有一个一模一样的仓库B，但是URL不同）
+将我们修改的代码push到自己github中的仓库B中
+pull request ，主人就会收到请求，并决定要不要接受你的代码
+---------------------
+作者：hong_lucy
+来源：CSDN
+原文：https://blog.csdn.net/u010089395/article/details/21100661
+版权 声明：本文为博主原创文章，转载请附上博文链接！
+        原来如此
+        * */
     }
+
 }
